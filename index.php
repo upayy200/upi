1febb6a6bdf4c3f0f9c0b9660e02da5d7a7d7f26
upay <luthfimuayyad200@gmail.com>
2024-06-06 10:38:16 +0700

    tambah

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <!-- struktur kepala -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website untuk pembelajaran WEB">
+    <title>TI 4A</title>
+</head> <!-- akhir struktur kepala -->
+<body> <!-- awal struktur tubuh (untuk membuat konten) -->
+    <h1>Pemrograman WEB 1</h1>
+    
+    <p> <!-- awal paragraph -->
+        Daftar Materi Pemrograman Web 1, Program Studi Teknik Informatika:<br>
+        <!--  <a> adalah tag untuk hyperlink, membuka halaman/file lain -->
+        <!--  target="_blank" adalah untuk membuka tab baru -->
+        <a href="menu.php" target="_blank">Menu</a>
+
+        <ol type="1">
+            <li>
+                <a href="html_dasar/heading.html">HTML Dasar - Heading</a>
+            </li>
+            <li>Pembuatan FORM</li>
+            <li>PHP Dasar</li>
+            <li>Pengiriman Data</li>
+            <li>Templating</li>
+        </ol>
+    </p> <!-- akhir paragraph -->
+</body><!-- akhi struktur tubuh (untuk membuat konten) -->
+</html>
---
+++ php_database/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "upi123";
+$db = "web1";
+
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// if($conn){
+//     echo "Koneksi berhasil";
+// }else{
+//     echo "Koneksi gagal".mysqli_errno($conn);
+// }
+
+
